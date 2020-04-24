6e02742f5e8c35ed726ea0519628e602329e5a7d
Meng <@mengyang41>
2020-04-24 11:23:04 -0400

    Updates

---
+++ precache-manifest.b70039942d3b2c8d992924a5bcda6147.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a05cdad64d07e7b37b0dea19e4953c2",
+    "url": "/reactWeatherApp/index.html"
+  },
+  {
+    "revision": "6cf638c20a0aa46a12ba",
+    "url": "/reactWeatherApp/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "be7353bd3b5a2f81cf17",
+    "url": "/reactWeatherApp/static/css/main.a052dc0e.chunk.css"
+  },
+  {
+    "revision": "6cf638c20a0aa46a12ba",
+    "url": "/reactWeatherApp/static/js/2.fdefbd3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactWeatherApp/static/js/2.fdefbd3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7353bd3b5a2f81cf17",
+    "url": "/reactWeatherApp/static/js/main.682513c8.chunk.js"
+  },
+  {
+    "revision": "a694fa917927ebe31345",
+    "url": "/reactWeatherApp/static/js/runtime-main.3ef89a64.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/reactWeatherApp/static/media/Montserrat-Bold.ade91f47.ttf"
+  }
+]);
